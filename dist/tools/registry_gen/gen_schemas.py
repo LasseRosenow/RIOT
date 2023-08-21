01b3822e2efda5c08309642208c8dd670ed7d14f
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2023-08-21 20:35:56 +0200

    dist/tools: add registry_gen skeleton

---
+++ dist/tools/registry_gen/gen_schemas.py
@@ -0,0 +1,11 @@
+# import yaml
+
+# with open("./definitions/0001_rgb_led.yaml", "r") as stream:
+#     try:
+#         print(yaml.safe_load(stream))
+#     except yaml.YAMLError as exc:
+#         print(exc)
+
+# f = open("./generated_schema.h", "a")
+# f.write("Now the file has more content!")
+# f.close()
