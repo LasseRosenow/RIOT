99aa25f486e8728b65915acaf8afdec8fc8f983f
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2023-08-21 20:20:12 +0200

    tests/unittests: add registry_storage tests

---
+++ tests/unittests/tests-registry_storage/tests-registry_storage.c
@@ -0,0 +1,185 @@
+/*
+ * Copyright (C) 2023 HAW Hamburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    unittests
+ * @brief       Unittests for the ``registry_storage`` module
+ * @{
+ *
+ * @file
+ *
+ * @author      Lasse Rosenow <lasse.rosenow@haw-hamburg.de>
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <float.h>
+#include <inttypes.h>
+#include <errno.h>
+#include "embUnit.h"
+#include "fmt.h"
+#include "assert.h"
+#include "vfs.h"
+#include "board.h"
+#include "mtd.h"
+#include "registry.h"
+#include "registry/storage.h"
+
+#include "tests-registry_storage.h"
+#include "registry/namespace/tests.h"
+#include "registry/namespace/tests/nested.h"
+
+#if IS_USED(MODULE_REGISTRY_NAMESPACE_TESTS_NESTED) || IS_ACTIVE(DOXYGEN)
+
+#define _TESTS_REGISTRY_LOAD_STORED_VALUE 60
+
+static registry_tests_nested_instance_t test_nested_instance_data = {
+    .parameter = 9,
+    .group_parameter = 5,
+};
+
+static registry_instance_t test_nested_instance = {
+#if IS_ACTIVE(CONFIG_REGISTRY_ENABLE_META_NAME)
+    .name = "test-nested-parameter-test",
+#endif /* CONFIG_REGISTRY_ENABLE_META_NAME */
+    .data = &test_nested_instance_data,
+    .commit_cb = NULL,
+};
+
+static int load(const registry_storage_instance_t *storage,
+                const load_cb_t load_cb);
+static int save(const registry_storage_instance_t *storage,
+                const registry_instance_t *instance,
+                const registry_parameter_t *parameter,
+                const registry_value_t *value);
+
+static registry_storage_t storage_test = {
+    .load = load,
+    .save = save,
+};
+
+static uint8_t storage_test_instance_data = 7;
+
+static registry_storage_instance_t storage_test_instance = {
+    .storage = &storage_test,
+    .data = &storage_test_instance_data,
+};
+
+static int load(const registry_storage_instance_t *storage,
+                const load_cb_t load_cb)
+{
+    if (storage == &storage_test_instance) {
+        uint8_t buf = _TESTS_REGISTRY_LOAD_STORED_VALUE;
+        return load_cb(&test_nested_instance, &registry_tests_nested_parameter, &buf, sizeof(buf));
+    }
+
+    return -EINVAL;
+}
+
+static int save(const registry_storage_instance_t *storage,
+                const registry_instance_t *instance,
+                const registry_parameter_t *parameter,
+                const registry_value_t *value)
+{
+    if (storage == &storage_test_instance &&
+        instance == &test_nested_instance &&
+        parameter == &registry_tests_nested_group_parameter &&
+        value->buf == &test_nested_instance_data.group_parameter &&
+        value->buf_len == sizeof(uint8_t) &&
+        value->type == REGISTRY_TYPE_UINT8) {
+
+        return 0;
+    }
+
+    return -EINVAL;
+}
+
+REGISTRY_ADD_STORAGE_SOURCE(storage_test_instance);
+REGISTRY_SET_STORAGE_DESTINATION(storage_test_instance);
+
+static void test_setup(void)
+{
+    /* init registry */
+    registry_init();
+
+    /* add schema instances */
+    registry_add_schema_instance(&registry_tests_nested, &test_nested_instance);
+}
+
+static void tests_registry_load(void)
+{
+    /* check if the registry_load function gets the correct input values internally */
+    TEST_ASSERT(registry_load() == 0);
+
+    /* check if the load_cb sets the value to the registry */
+    registry_value_t output;
+
+    registry_get(&test_nested_instance, &registry_tests_nested_parameter, &output);
+    TEST_ASSERT_EQUAL_INT(_TESTS_REGISTRY_LOAD_STORED_VALUE, *(uint8_t *)output.buf);
+}
+
+static void tests_registry_save_parameter(void)
+{
+    TEST_ASSERT_EQUAL_INT(0, registry_save_parameter(&test_nested_instance,
+                                                     &registry_tests_nested_group_parameter));
+}
+
+static void tests_registry_save_group(void)
+{
+    TEST_ASSERT_EQUAL_INT(0, registry_save_group(&test_nested_instance,
+                                                 &registry_tests_nested_group));
+}
+
+static void tests_registry_save_instance(void)
+{
+    TEST_ASSERT_EQUAL_INT(0, registry_save_instance(&test_nested_instance));
+}
+
+static void tests_registry_save_schema(void)
+{
+    TEST_ASSERT_EQUAL_INT(0, registry_save_schema(&registry_tests_nested));
+}
+
+static void tests_registry_save_namespace(void)
+{
+    TEST_ASSERT_EQUAL_INT(0, registry_save_namespace(&registry_tests));
+}
+
+static void tests_registry_save_all(void)
+{
+    TEST_ASSERT_EQUAL_INT(0, registry_save());
+}
+
+Test *tests_registry_storage_tests(void)
+{
+    EMB_UNIT_TESTFIXTURES(fixtures) {
+        /* load */
+        new_TestFixture(tests_registry_load),
+        /* save */
+        new_TestFixture(tests_registry_save_parameter),
+        new_TestFixture(tests_registry_save_group),
+        new_TestFixture(tests_registry_save_instance),
+        new_TestFixture(tests_registry_save_schema),
+        new_TestFixture(tests_registry_save_namespace),
+        new_TestFixture(tests_registry_save_all),
+    };
+
+    EMB_UNIT_TESTCALLER(registry_storage_tests, test_setup, NULL, fixtures);
+
+    return (Test *)&registry_storage_tests;
+}
+
+void tests_registry_storage(void)
+{
+    TESTS_RUN(tests_registry_storage_tests());
+}
+
+#endif
+
+/** @} */
---
+++ tests/unittests/tests-registry_storage/tests-registry_storage.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2023 HAW Hamburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    unittests
+ * @brief       Unittests for the ``registry_storage`` module
+ * @{
+ *
+ * @file
+ *
+ * @author      Lasse Rosenow <lasse.rosenow@haw-hamburg.de>
+ */
+
+#ifndef TESTS_REGISTRY_STORAGE_H
+#define TESTS_REGISTRY_STORAGE_H
+
+#include "embUnit.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief The entry point of this test suite.
+ */
+void tests_registry_storage(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TESTS_REGISTRY_STORAGE_H */
+/** @} */
